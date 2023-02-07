538e89a0927a076db0107a8576b7bfac89722e58
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-02-07 17:06:00 -0600

    Add\ Padding in visit page

--- resources/views/front/visit/sections/places.blade.php
+++ resources/views/front/visit/sections/places.blade.php
@@ -1,44 +1,47 @@
 <section>
 
     <div class="space-y"></div>
-    <div class="bg-main-light flex md:grid md:grid-cols-2 flex-col-reverse">
+    <div class="p-20 bg-motive">
 
-        <div class="px-10 md:mx-auto my-11">
-            <h2 class="font-bold text-xs uppercase mb-10">
-                Hotel Ronda Minerva
-            </h2>
+        <div class="shadow-card flex md:grid md:grid-cols-2 flex-col-reverse">
 
-            <h3 class="font-bold text-3xl mb-10">EN GUADALAJARA</h3>
+            <div class="px-10 md:mx-auto my-11">
+                <h2 class="font-bold text-xs uppercase mb-10">
+                    Hotel Ronda Minerva
+                </h2>
 
-            <p class="text-sm leading-6">
-                Ronda Minerva Hotel es una joya de la cultura de Jalisco, ubicado en el corazón
-de la capital tapatía, es un referente de la historia de la ciudad. A menos de 3
-cuadras de La Minerva, uno de los sitios más representativos del país. Bajando al centro, a menos de 800 metros se encuentra Av. Chapultepec, el lugar en donde estar si se trata de la vida nocturna.
-            </p>
+                <h3 class="font-bold text-3xl mb-10">EN GUADALAJARA</h3>
 
-            <div class="space-y"></div>
-            <ul class="hidden">
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> 77 Habitaciones</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Wi-Fi Gratis</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Restaurante</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Recepción 24 Hrs.</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Seguridad Privada</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Periódicos gratuitos en el lobby</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Estacionamiento gratis</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Traslado al aeropuerto</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Personal multilingüe</li>
-                <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Artículos de tocador gratuitos</li>
-            </ul>
+                <p class="text-sm leading-6">
+                    Ronda Minerva Hotel es una joya de la cultura de Jalisco, ubicado en el corazón
+    de la capital tapatía, es un referente de la historia de la ciudad. A menos de 3
+    cuadras de La Minerva, uno de los sitios más representativos del país. Bajando al centro, a menos de 800 metros se encuentra Av. Chapultepec, el lugar en donde estar si se trata de la vida nocturna.
+                </p>
 
-            <div class="my-12 hidden">
-                <button class="w-36 h-12 font-bold bg-main text-white text-sm">ver todos</button>
+                <div class="space-y"></div>
+                <ul class="hidden">
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> 77 Habitaciones</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Wi-Fi Gratis</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Restaurante</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Recepción 24 Hrs.</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Seguridad Privada</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Periódicos gratuitos en el lobby</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Estacionamiento gratis</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Traslado al aeropuerto</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Personal multilingüe</li>
+                    <li class="text-xl mb-2"> <span class="font-bold text-3xl text-main">•</span> Artículos de tocador gratuitos</li>
+                </ul>
+
+                <div class="my-12 hidden">
+                    <button class="w-36 h-12 font-bold bg-main text-white text-sm">ver todos</button>
+                </div>
             </div>
-        </div>
 
-        <div>
-            <iframe class="w-full max-h-96 h-96 object-cover" src="https://www.google.com/maps/d/embed?mid=1tbA2--WSPlThEvsIYTyEp5hQcH17NnQ&ehbc=2E312F" width="640" height="480"></iframe>
-            <iframe class="w-full max-h-[630px] h-full object-cover hidden" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=Av.%20Circ.%20Jorge%20%C3%81lvarez%20del%20Castillo%201482%2C%20Chapultepec%20Country%2C%2044620&amp;t=m&amp;z=15&amp;output=embed&amp;iwloc=near" title="Av. Circ. Jorge Álvarez del Castillo 1482, Chapultepec Country, 44620" aria-label="Av. Circ. Jorge Álvarez del Castillo 1482, Chapultepec Country, 44620"></iframe>
-        </div>
+            <div>
+                <iframe class="w-full max-h-96 h-96 object-cover" src="https://www.google.com/maps/d/embed?mid=1tbA2--WSPlThEvsIYTyEp5hQcH17NnQ&ehbc=2E312F" width="640" height="480"></iframe>
+            </div>
 
+        </div>
     </div>
+
 </section>
