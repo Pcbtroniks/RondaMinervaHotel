0958e1e77d4e9975ce90ed5e476e6aa0e6005d5a
Jean <jeanmacario048@gmail.com>
2025-05-05 11:48:42 -0600

    ADD\ New restaurant menus added

--- resources/views/front/restaurant/sections/links.blade.php
+++ resources/views/front/restaurant/sections/links.blade.php
@@ -2,13 +2,23 @@
 
     <div class="px-4 flex flex-col items-center pb-16">
 
-        <a target="_blank" href="https://rondaminervahotel.com/menu/restaurant-menu.pdf" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+        <a target="_blank" href="{{ asset('assets') }}/menus/al-eden-menu-desayunos.pdf" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
             <div class="w-[4.5rem]">
                 <img class="w-full" src="{{ asset('assets/r.svg') }}" alt="Logo restaurant">
             </div>
             <div class="ml-2 sm:ml-6">
-                <h2 class="sm:text-sm uppercase font-medium">Restaurant Al Eden</h2>
-                <p class="text-xs">En nuestro restaurante ‘‘Al Edén’’ podrás degustar de deliciosos platillos.</p>
+                <h2 class="sm:text-sm uppercase font-medium">Restaurante Al Eden Desayunos</h2>
+                <p class="text-xs">En nuestro restaurante ‘‘Al Edén’’ podrás degustar de deliciosos platillos. haz <span class="text-ronda-orange font-bold hover:text-ronda-orange/50 transition-colors duration-150">click aqui</span> para ver el menu de desayunos</p>
+            </div>
+        </a>
+
+        <a target="_blank" href="{{ asset('assets') }}/menus/al-eden-menu-comidas-y-cenas.pdf" class="bg-white flex items-center my-5 py-4 px-2 sm:px-4 rounded-full shadow-card-hard sm:w-[29.25rem] w-[20.5rem]">
+            <div class="w-[4.5rem]">
+                <img class="w-full" src="{{ asset('assets/r.svg') }}" alt="Logo restaurant">
+            </div>
+            <div class="ml-2 sm:ml-6">
+                <h2 class="sm:text-sm uppercase font-medium">Restaurante Al Eden Comidas y Cenas</h2>
+                <p class="text-xs">En nuestro restaurante ‘‘Al Edén’’ podrás degustar de deliciosos platillos. haz <span class="text-ronda-orange font-bold hover:text-ronda-orange/50 transition-colors duration-150">click aqui</span> para ver el menu de comidas y cenas</p>
             </div>
         </a>
 
